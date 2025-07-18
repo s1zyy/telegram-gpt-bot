cc37703359e90749e64045d15eac505472044d9c
Vlad <sav320801@gmail.com>
2025-07-18 14:07:20 +0100

    main menu

--- src/main/java/selfprojects/my_telegram_gpt_bot/updateConsumer.java
+++ src/main/java/selfprojects/my_telegram_gpt_bot/updateConsumer.java
@@ -6,8 +6,12 @@ import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
+import java.util.List;
 
 
 @Component
@@ -26,7 +30,48 @@ public class updateConsumer implements LongPollingSingleThreadUpdateConsumer {
         var chatId = update.getMessage().getChatId();
         String message = update.getMessage().getText();
         sendMessage(chatId, message);
+
+        if(message.equals("/start")){
+            sendMainMenu(chatId);
+        }
     }
+
+    private void sendMainMenu(Long chatId) {
+
+        SendMessage sendMessage = SendMessage
+                .builder()
+                .chatId(chatId)
+                .text("This is your main menu!")
+                .build();
+
+        var button1 = InlineKeyboardButton.builder()
+                .text("Ask GPT a question")
+                .callbackData("gpt")
+                .build();
+        var button2 = InlineKeyboardButton.builder()
+                .text("About this project")
+                .callbackData("project")
+                .build();
+        var button3 = InlineKeyboardButton.builder()
+                .text("About author")
+                .callbackData("author")
+                .build();
+
+        List<InlineKeyboardRow> inlineKeyboardRows = List.of(
+                new InlineKeyboardRow(button1),
+                new InlineKeyboardRow(button2),
+                new InlineKeyboardRow(button3)
+        );
+
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup(inlineKeyboardRows);
+        sendMessage.setReplyMarkup(markup);
+        try {
+            telegramClient.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     public void sendMessage(Long chatId, String message){
 
