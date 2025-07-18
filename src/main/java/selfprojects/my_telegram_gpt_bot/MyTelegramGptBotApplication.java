aa70dfc3666e5afa31a9e1c252fdd0b2f8f88aaa
Vlad <sav320801@gmail.com>
2025-07-18 12:41:56 +0100

    innit commit

---
+++ src/main/java/selfprojects/my_telegram_gpt_bot/MyTelegramGptBotApplication.java
@@ -0,0 +1,13 @@
+package selfprojects.my_telegram_gpt_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTelegramGptBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyTelegramGptBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/selfprojects/my_telegram_gpt_bot/MyTelegramGptBotApplicationTests.java
@@ -0,0 +1,13 @@
+package selfprojects.my_telegram_gpt_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTelegramGptBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
